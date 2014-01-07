0ba9e8d1012196f94d451738e1a5be7014ffd56e
Mark Greenway <mgreenway@gmail.com>
2014-01-07 16:56:53 -0500

    All the failing tests

---
+++ TDandD/describe_CharacterDexterity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSpec;
+
+namespace TDandD
+{
+    class describe_CharacterDexterity : nspec
+    {
+        Character bob;
+        private const int default_ArmorClass = Character.DEFAULT_ARMORCLASS;
+
+        void before_each()
+        {
+            bob = new Character();
+        }
+        void when_Dexterity_modifier_applies()
+        {
+            context["given Dexterity Value is 20"] = () =>
+            {
+                before = () => bob.Dexterity.Value = 20;
+                it["ArmorClass should be increased by 5"] = () => bob.ArmorClass.should_be(default_ArmorClass + 5);
+            };
+
+            context["given Dexterity Value is 1"] = () =>
+            {
+                before = () => bob.Dexterity.Value = 1;
+                it["ArmorClass should be decreased by 5"] = () => bob.ArmorClass.should_be(default_ArmorClass - 5);
+            };
+
+            context["given Dexterity Value is 10"] = () =>
+            {
+                before = () => bob.Dexterity.Value = 10;
+                it["ArmorClass should remain the same"] = () => bob.ArmorClass.should_be(default_ArmorClass);
+            };
+        }
+    }
+}
